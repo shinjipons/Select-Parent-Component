b8314be9c8bf8e306ecc63a3c5e8fe0f9ee4ee58
Shinji Pons <github@shinjipons.com>
2025-07-28 21:21:10 +0200

    Working plugin

--- code.js
+++ code.js
@@ -1,1 +1,46 @@
-throw new Error("This plugin template uses TypeScript. Follow the instructions in `README.md` to generate `code.js`.")
+"use strict";
+// Select Nearest Parent Component Instance
+// Check if any nodes are selected
+if (figma.currentPage.selection.length === 0) {
+    figma.notify("Please select at least one layer.");
+    figma.closePlugin();
+}
+else {
+    let validParentInstances = [];
+    // Process each selected node
+    for (let selectedNode of figma.currentPage.selection) {
+        let node = selectedNode;
+        let foundInstance = null;
+        // Traverse up the parent hierarchy
+        while (node.parent && node.parent.type !== "PAGE") {
+            if (node.parent.type === "DOCUMENT") {
+                break;
+            }
+            node = node.parent;
+            if (node.type === "INSTANCE") {
+                foundInstance = node;
+                break;
+            }
+        }
+        // Add to valid instances if found
+        if (foundInstance) {
+            validParentInstances.push(foundInstance);
+        }
+    }
+    if (validParentInstances.length > 0) {
+        // Remove duplicates (in case multiple selected items have the same parent)
+        const uniqueInstances = validParentInstances.filter((instance, index, self) => index === self.findIndex(i => i.id === instance.id));
+        figma.currentPage.selection = uniqueInstances;
+        figma.viewport.scrollAndZoomIntoView(uniqueInstances);
+        if (uniqueInstances.length === 1) {
+            figma.notify(`Selected parent instance: ${uniqueInstances[0].name}`);
+        }
+        else {
+            figma.notify(`Selected ${uniqueInstances.length} parent instances`);
+        }
+    }
+    else {
+        figma.notify("No parent component instances found for any selected items.");
+    }
+    figma.closePlugin();
+}
--- code.ts
+++ code.ts
@@ -1,27 +1,51 @@
 // Select Nearest Parent Component Instance
 
-// Ensure exactly one node is selected
-if (figma.currentPage.selection.length !== 1) {
-  figma.notify("Please select exactly one layer.");
+// Check if any nodes are selected
+if (figma.currentPage.selection.length === 0) {
+  figma.notify("Please select at least one layer.");
   figma.closePlugin();
 } else {
-  let node = figma.currentPage.selection[0];
-  let foundInstance: InstanceNode | null = null;
+  let validParentInstances: InstanceNode[] = [];
 
-  while (node.parent && node.parent.type !== "PAGE") {
-    node = node.parent;
-    if (node.type === "INSTANCE") {
-      foundInstance = node;
-      break;
+  // Process each selected node
+  for (let selectedNode of figma.currentPage.selection) {
+    let node = selectedNode;
+    let foundInstance: InstanceNode | null = null;
+
+    // Traverse up the parent hierarchy
+    while (node.parent && node.parent.type !== "PAGE") {
+      if (node.parent.type === "DOCUMENT") {
+        break;
+      }
+      node = node.parent as SceneNode;
+      if (node.type === "INSTANCE") {
+        foundInstance = node;
+        break;
+      }
+    }
+
+    // Add to valid instances if found
+    if (foundInstance) {
+      validParentInstances.push(foundInstance);
     }
   }
 
-  if (foundInstance) {
-    figma.currentPage.selection = [foundInstance];
-    figma.viewport.scrollAndZoomIntoView([foundInstance]);
-    figma.notify(`Selected parent instance: ${foundInstance.name}`);
+  if (validParentInstances.length > 0) {
+    // Remove duplicates (in case multiple selected items have the same parent)
+    const uniqueInstances = validParentInstances.filter((instance, index, self) =>
+      index === self.findIndex(i => i.id === instance.id)
+    );
+
+    figma.currentPage.selection = uniqueInstances;
+    figma.viewport.scrollAndZoomIntoView(uniqueInstances);
+
+    if (uniqueInstances.length === 1) {
+      figma.notify(`Selected parent instance: ${uniqueInstances[0].name}`);
+    } else {
+      figma.notify(`Selected ${uniqueInstances.length} parent instances`);
+    }
   } else {
-    figma.notify("No parent component instance found.");
+    figma.notify("No parent component instances found for any selected items.");
   }
 
   figma.closePlugin();
